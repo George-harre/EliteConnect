40d72db72e37126998c4a39dc7234fad271db2b2
George-harre <161688720+George-harre@users.noreply.github.com>
2026-07-14 16:13:27 +0300

    Connect frontend to deployed Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://eliteconnect-x6xw.onrender.com/api",
     headers: {
         "Content-Type": "application/json"
     }
@@ -17,6 +17,7 @@ api.interceptors.request.use((config) => {
     }
 
     return config;
+
 });
 
 export default api;
