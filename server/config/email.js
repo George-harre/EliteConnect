7b6d4836ccd9aa3e5600b72e021810b11b6a138c
George-harre <161688720+George-harre@users.noreply.github.com>
2026-07-18 17:11:51 +0300

    Switch email service from Gmail SMTP to Brevo SMTP

--- server/config/email.js
+++ server/config/email.js
@@ -2,21 +2,13 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
 
-    host: "smtp.gmail.com",
+    host: "smtp-relay.brevo.com",
 
     port: 587,
 
     secure: false,
 
     requireTLS: true,
-
-    connectionTimeout: 30000,
-
-    greetingTimeout: 30000,
-
-    socketTimeout: 30000,
-
-    family: 4,
 
     auth: {
 
@@ -30,10 +22,8 @@ const transporter = nodemailer.createTransport({
 
 console.log("=================================");
 console.log("SMTP CONFIG LOADED");
-console.log("Host:", "smtp.gmail.com");
+console.log("Host:", "smtp-relay.brevo.com");
 console.log("Port:", 587);
-console.log("Secure:", false);
-console.log("RequireTLS:", true);
 console.log("=================================");
 
 module.exports = transporter;
